5c207ef2921eeb7b1a1ce4430a638595ca66311e
elifarslancelik <elifnurarslnclk@gmail.com>
2025-05-06 14:50:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 # VENV'i aktive et
-source /Users/elifarslancelik/Desktop/yz/venv/bin/activate
+source /Users/username/Desktop/yz/venv/bin/activate
 
 # Python scriptini çalıştır
-python3 /Users/elifarslancelik/Desktop/yz/yz.py
+python3 /Users/username/Desktop/yz/yz.py
 
 # Hata olsa bile venv'den çık
 deactivate
